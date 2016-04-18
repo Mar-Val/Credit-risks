7c6897475b09d4e12a8e79a5f47352aadb29b758
Marina <Marina>
2016-04-18 15:11:35 +0300

    ROC Gini

--- Gini.R
+++ Gini.R
@@ -1,31 +1,47 @@
 You need to sort table decreasing by score. The ranged row of numbers is a probabilities to become 1! The lower the score - the less 
 probability to become default (in this case 1)
-data_1 <- read.table(file = file.choose(), header = TRUE, sep = ";")
-data_sort<-data_1[order(-data_1$FACT),] 
-data_sort<-data_1[order(data_1$FACT),] 
-N=length(which(data_1$TAR==0))
-P=length(which(data_1$TAR==1))
+
+data_2 <- read.table(file = file.choose(), header = TRUE, sep = ";")
+
+
+
+Gini_MarVal <-function (Ran,Tar,dir) {
+data_1<-data.frame(Ran,Tar)
+data_sort<-data_1[order(dir*data_1$Ran),] 
+data_sort
+N=length(which(data_1$Tar==0))
+P=length(which(data_1$Tar==1))
 TPR<-c()
 FPR<-c()
 TP=0
 FN=0
-for (i in data_sort$TAR){
+for (i in data_sort$Tar){
   if (i==1) {TP=TP+1} 
   else {FN=FN+1}
   TPR<-c(TPR, TP/P)
   FPR<-c(FPR, FN/N)
 }
 
-plot(FPR,TPR, type="l")
+TPR
+FPR
+plot(FPR,TPR, type="l",col="red",xlab="FPR",ylab="TPR")
+par(new=TRUE)
+plot(c(0,1),c(0,1),type="l",axes=FALSE,xlab="FPR",ylab="TPR")
 t<-data.frame(FPR,TPR)
 s<-c()
 j=0
 num=nrow(t)-1
 for (j in 1:num){
-  s[j]=(FPR[j]+FPR[j+1])*(TPR[j+1]-TPR[j])/2
+  s[j]=(FPR[j+1]-FPR[j])*(TPR[j+1]+TPR[j])/2
 }
 AUC=sum(s)
-AUC
-Gini=2*(AUC-0.5)
 Gini
+Gini=2*(AUC-0.5)
+return(Gini)
+}
+
+do.call("Gini_MarVal", list( Ran=data_2$R, Tar=data_2$T, dir=1))
+
+
+
 
